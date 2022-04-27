8e9192e4c994ce46a45555487e48fb885f194fd2
PiyankaBadole <94530938+PiyankaBadole@users.noreply.github.com>
2022-04-27 11:03:24 +0530

    Add files via upload

---
+++ day4/Q2,3/script.js
@@ -0,0 +1,17 @@
+let req = new XMLHttpRequest();
+
+req.onreadystatechange = function(){
+    if(req.status == 200){
+        res = JSON.parse(this.responseText)
+        res.forEach(element => {
+            console.log(element.flag, element.name,element.region,element.subregion,element.population)
+        });
+        }
+        
+}
+
+
+req.open('GET','https://restcountries.com/v2/all')
+
+
+req.send();
